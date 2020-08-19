ebc6464589dcf129cc8c0162dbd7db722bd87e0a
qiualu <1101938064@qq.com>
2020-08-19 23:35:19 +0800

    8.19ye

--- Qt/Qt07_dialog_file/widget.cpp
+++ Qt/Qt07_dialog_file/widget.cpp
@@ -1,11 +1,23 @@
 ﻿#include "widget.h"
 #include "ui_widget.h"
 
+
+#include<QMessageBox>
+#include<QFontDialog>
+#include<QColorDialog>
+#include<QPalette>
+#include<QInputDialog>
+#include<QFileDialog>
+
+#include <QTextStream>
+
 Widget::Widget(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::Widget)
 {
     ui->setupUi(this);
+    p_dialog = NULL;
+    Init();
 }
 
 Widget::~Widget()
@@ -13,14 +25,121 @@ Widget::~Widget()
     delete ui;
 }
 
-// 打开文件
-void Widget::on_pushButton_clicked()
+void Widget::Init(){
+    myFile = new QFile(this);
+}
+
+// 设置字体 msg dislog
+void Widget::on_pushButton_3_clicked()
 {
+    QMessageBox::warning(this,"warning","xxxxxxx",
+                         QMessageBox::Yes|QMessageBox::No);
 
 }
 
+void Widget::on_color_dialog_clicked()
+{
+    QColor mycolor = QColorDialog::getColor(Qt::red,this,"get color");
+    if(mycolor.isValid()){
+        QPalette pe = ui->label->palette();
+        pe.setColor(QPalette::WindowText,mycolor);
+        ui->label->setPalette(pe);
+    }
+}
+
+void Widget::on_font_dialog_clicked()
+{
+    bool ok;
+    QFont serifFont("Lobster",22,QFont::Bold); //设置
+    QFont myfont = QFontDialog::getFont(&ok,serifFont,this,"get font",QFontDialog::ScalableFonts);
+    if(ok){
+        ui->label->setFont(myfont);
+    }
+
+}
+
+void Widget::on_input_dialog_clicked()
+{
+    int i = QInputDialog::getInt(this,"get int","年龄",1,1,100,1);
+    qDebug() << i<< " 大小吧 ";
+}
+
+void Widget::on_progress_dialog_clicked()
+{
+    if(p_dialog == NULL){
+        step = 0;
+        p_dialog = new QProgressDialog(this);
+        p_dialog->setRange(0,100);
+        connect(p_dialog,SIGNAL(canceled()),this,SLOT(doProcessCancel()));
+
+        myTimer =  new QTimer(this);
+        connect(myTimer,SIGNAL(timeout()),this,SLOT(doProcessTimeOut()));
+        myTimer->start(100);
+    }
 
 
+}
+void Widget::doProcessCancel(){
+    qDebug() << " doProcessCancel ";
+    myTimer->stop();
+    delete p_dialog;
+    p_dialog = NULL;
+}
+void Widget::doProcessTimeOut(){
+    qDebug() << " doProcessTimeOut ";
+    p_dialog->setValue(step);
+    step++;
+    if(step==101){
+        myTimer->stop();
+        delete p_dialog;
+        p_dialog = NULL;
+        step = 0;
+    }
+}
 
 
+// 打开文件
+void Widget::on_pushButton_clicked()
+{
+    QString filename = QFileDialog::getOpenFileName(this,"get file","../Qt07_dialog_file/",
+                                                    "SOURCES(*.cpp);;HEADERS(*.h);;FORMS(*.ui)");
+    qDebug() << " filename "<< filename << endl;
+    if(filename.isEmpty()){
+        QMessageBox::warning(this,"warning","file can`t select");
+        return;
+    }
 
+    myFile->setFileName(filename);
+    bool ret = myFile->open(QIODevice::ReadOnly|QIODevice::Text);
+    if(!ret){
+        QMessageBox::warning(this,"warning","open failed");
+        return;
+    }
+//    while(!myFile->atEnd()){
+
+//        QByteArray ba = myFile->read(1024);
+//        QString str = QString(ba);
+//        ui->textEdit->append(str);
+
+//        QByteArray ba = myFile->readLine();
+//        QString str = QString(ba);
+//        ui->textEdit->append(str);
+
+//        QByteArray ba = myFile->readAll();
+//        QString str = QString(ba);
+//        ui->textEdit->append(str);
+
+//    }
+    // 文件流形式
+    QTextStream text(myFile);
+    while(!text.atEnd()){
+        QString str = text.readAll();
+        ui->textEdit->append(str);
+    }
+
+}
+// 保存文件
+void Widget::on_save_file_clicked()
+{
+
+}
--- Qt/Qt07_dialog_file/widget.h
+++ Qt/Qt07_dialog_file/widget.h
@@ -1,6 +1,10 @@
 ﻿#ifndef WIDGET_H
 #define WIDGET_H
-
+#include <QDebug>
+#include<QProgressDialog>
+#include<QTimer>
+#include<QFile>
+#include <QByteArray>
 
 // MSVC中文乱码
 #if _MSC_VER >= 1600
@@ -21,10 +25,35 @@ public:
     Widget(QWidget *parent = nullptr);
     ~Widget();
 
+
+     QProgressDialog *p_dialog;
+     QTimer *myTimer;
+     int step;
+
 private slots:
     void on_pushButton_clicked();
 
+    void on_pushButton_3_clicked();
+
+    void on_color_dialog_clicked();
+
+    void on_font_dialog_clicked();
+
+    void on_input_dialog_clicked();
+
+    void on_progress_dialog_clicked();
+
+    void doProcessCancel();
+    void doProcessTimeOut();
+
+    void on_save_file_clicked();
+
 private:
     Ui::Widget *ui;
+
+    QFile *myFile;
+    void Init();
+
+
 };
 #endif // WIDGET_H
